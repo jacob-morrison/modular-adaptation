0be37056e0f684c5f4ef44d964855053e2505dd8
jacob-morrison <jacobm@allenai.org>
2024-01-18 16:27:32 -0800

    more merges for subsets

--- scripts/mergekit-configs/merge-models.sh
+++ scripts/mergekit-configs/merge-models.sh
@@ -6,9 +6,16 @@
 # mergekit-yaml ties-no-robots-3-experts.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-3-experts-ties --cuda
 # mergekit-yaml dare-linear-no-robots-3-experts.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-3-experts-dare-linear --cuda
 # mergekit-yaml dare-ties-no-robots-3-experts.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-3-experts-dare-ties --cuda
-mergekit-yaml ties-no-robots-all-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-ties --cuda
-mergekit-yaml ties-no-robots-all-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-ties-weighted --cuda
-mergekit-yaml dare-ties-no-robots-all-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-ties --cuda
-mergekit-yaml dare-ties-no-robots-all-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-ties-weighted --cuda
-mergekit-yaml dare-linear-no-robots-all-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-linear --cuda
-mergekit-yaml dare-linear-no-robots-all-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-linear-weighted --cuda
+# mergekit-yaml ties-no-robots-all-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-ties --cuda
+# mergekit-yaml ties-no-robots-all-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-ties-weighted --cuda
+# mergekit-yaml dare-ties-no-robots-all-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-ties --cuda
+# mergekit-yaml dare-ties-no-robots-all-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-ties-weighted --cuda
+# mergekit-yaml dare-linear-no-robots-all-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-linear --cuda
+# mergekit-yaml dare-linear-no-robots-all-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-all-subsets-dare-linear-weighted --cuda
+
+mergekit-yaml ties-no-robots-top-4-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-top-4-subsets-ties --cuda
+mergekit-yaml ties-no-robots-top-4-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-top-4-subsets-ties-weighted --cuda
+mergekit-yaml dare-ties-no-robots-top-4-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-top-4-subsets-dare-ties --cuda
+mergekit-yaml dare-ties-no-robots-top-4-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-top-4-subsets-dare-ties-weighted --cuda
+mergekit-yaml dare-linear-no-robots-top-4-subsets.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-top-4-subsets-dare-linear --cuda
+mergekit-yaml dare-linear-no-robots-top-4-subsets-weighted.yml /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/merge-top-4-subsets-dare-linear-weighted --cuda
