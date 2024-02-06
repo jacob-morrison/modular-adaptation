dbceb247e22a26f3fd6e6e6712614e04a26ace46
jacob-morrison <jacobm@allenai.org>
2024-02-06 11:16:53 -0500

    train other baselines on 8 gpus

--- scripts/finetune_baseline_models_with_accelerate.sh
+++ scripts/finetune_baseline_models_with_accelerate.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 MODEL_SIZE=7B
-NUM_GPUS=4
+NUM_GPUS=8
 BATCH_SIZE_PER_GPU=2
 TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
@@ -10,7 +10,57 @@ echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_G
 TRAINING_DATA_DIR=/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/
 BASE_MODEL_PATH=/net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B
 
-# SKILL ADDITION: ONLY code and wizardlm
+# # SKILL ADDITION: ONLY code and wizardlm
+# accelerate launch \
+#     --mixed_precision bf16 \
+#     --num_machines 1 \
+#     --num_processes $NUM_GPUS \
+#     --use_deepspeed \
+#     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+#     open_instruct/finetune.py \
+#     --model_name_or_path ${BASE_MODEL_PATH} \
+#     --use_flash_attn \
+#     --tokenizer_name ${BASE_MODEL_PATH} \
+#     --use_slow_tokenizer \
+#     --train_file ${TRAINING_DATA_DIR}tulu_v2_filtered_only_code_alpaca_and_wizardlm.jsonl \
+#     --max_seq_length 2048 \
+#     --preprocessing_num_workers 16 \
+#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+#     --learning_rate 2e-5 \
+#     --lr_scheduler_type linear \
+#     --warmup_ratio 0.03 \
+#     --weight_decay 0. \
+#     --num_train_epochs 2 \
+#     --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/skill_addition/llama_2_7b-only_code_alpaca_and_wizardlm/ \
+#     --logging_steps 1
+
+# # DOMAIN ADDITION: ONLY science
+# accelerate launch \
+#     --mixed_precision bf16 \
+#     --num_machines 1 \
+#     --num_processes $NUM_GPUS \
+#     --use_deepspeed \
+#     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+#     open_instruct/finetune.py \
+#     --model_name_or_path ${BASE_MODEL_PATH} \
+#     --use_flash_attn \
+#     --tokenizer_name ${BASE_MODEL_PATH} \
+#     --use_slow_tokenizer \
+#     --train_file ${TRAINING_DATA_DIR}science_filtered_data.jsonl \
+#     --max_seq_length 2048 \
+#     --preprocessing_num_workers 16 \
+#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+#     --learning_rate 2e-5 \
+#     --lr_scheduler_type linear \
+#     --warmup_ratio 0.03 \
+#     --weight_decay 0. \
+#     --num_train_epochs 2 \
+#     --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/skill_addition/llama_2_7b-only_tulu_science_subset/ \
+#     --logging_steps 1
+
+# SKILL ADDITION: NO code and wizardlm
 accelerate launch \
     --mixed_precision bf16 \
     --num_machines 1 \
@@ -22,7 +72,7 @@ accelerate launch \
     --use_flash_attn \
     --tokenizer_name ${BASE_MODEL_PATH} \
     --use_slow_tokenizer \
-    --train_file ${TRAINING_DATA_DIR}tulu_v2_filtered_only_code_alpaca_and_wizardlm.jsonl \
+    --train_file ${TRAINING_DATA_DIR}tulu_v2_filtered_minus_wizardlm_and_code_alpaca.jsonl \
     --max_seq_length 2048 \
     --preprocessing_num_workers 16 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
@@ -32,10 +82,10 @@ accelerate launch \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
     --num_train_epochs 2 \
-    --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/skill_addition/llama_2_7b-only_code_alpaca_and_wizardlm/ \
+    --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/skill_addition/llama_2_7b-tulu_no_code_alpaca_or_wizardlm/ \
     --logging_steps 1
 
-# DOMAIN ADDITION: ONLY science
+# DOMAIN ADDITION: science
 accelerate launch \
     --mixed_precision bf16 \
     --num_machines 1 \
@@ -47,7 +97,7 @@ accelerate launch \
     --use_flash_attn \
     --tokenizer_name ${BASE_MODEL_PATH} \
     --use_slow_tokenizer \
-    --train_file ${TRAINING_DATA_DIR}science_filtered_data.jsonl \
+    --train_file ${TRAINING_DATA_DIR}tulu_v2_filtered_minus_science.jsonl \
     --max_seq_length 2048 \
     --preprocessing_num_workers 16 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
@@ -57,80 +107,30 @@ accelerate launch \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
     --num_train_epochs 2 \
-    --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/skill_addition/llama_2_7b-only_tulu_science_subset/ \
+    --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/domain_addition/llama_2_7b-tulu_no_science/ \
     --logging_steps 1
 
-# # SKILL ADDITION: NO code and wizardlm
-# accelerate launch \
-#     --mixed_precision bf16 \
-#     --num_machines 1 \
-#     --num_processes $NUM_GPUS \
-#     --use_deepspeed \
-#     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-#     open_instruct/finetune.py \
-#     --model_name_or_path ${BASE_MODEL_PATH} \
-#     --use_flash_attn \
-#     --tokenizer_name ${BASE_MODEL_PATH} \
-#     --use_slow_tokenizer \
-#     --train_file ${TRAINING_DATA_DIR}tulu_v2_filtered_minus_wizardlm_and_code_alpaca.jsonl \
-#     --max_seq_length 2048 \
-#     --preprocessing_num_workers 16 \
-#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
-#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-#     --learning_rate 2e-5 \
-#     --lr_scheduler_type linear \
-#     --warmup_ratio 0.03 \
-#     --weight_decay 0. \
-#     --num_train_epochs 2 \
-#     --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/skill_addition/llama_2_7b-tulu_no_code_alpaca_or_wizardlm/ \
-#     --logging_steps 1
-
-# # DOMAIN ADDITION: science
-# accelerate launch \
-#     --mixed_precision bf16 \
-#     --num_machines 1 \
-#     --num_processes $NUM_GPUS \
-#     --use_deepspeed \
-#     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-#     open_instruct/finetune.py \
-#     --model_name_or_path ${BASE_MODEL_PATH} \
-#     --use_flash_attn \
-#     --tokenizer_name ${BASE_MODEL_PATH} \
-#     --use_slow_tokenizer \
-#     --train_file ${TRAINING_DATA_DIR}tulu_v2_filtered_minus_science.jsonl \
-#     --max_seq_length 2048 \
-#     --preprocessing_num_workers 16 \
-#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
-#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-#     --learning_rate 2e-5 \
-#     --lr_scheduler_type linear \
-#     --warmup_ratio 0.03 \
-#     --weight_decay 0. \
-#     --num_train_epochs 2 \
-#     --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/domain_addition/llama_2_7b-tulu_no_science/ \
-#     --logging_steps 1
-
-# # BASELINE: Tulu 2 7B
-# accelerate launch \
-#     --mixed_precision bf16 \
-#     --num_machines 1 \
-#     --num_processes $NUM_GPUS \
-#     --use_deepspeed \
-#     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-#     open_instruct/finetune.py \
-#     --model_name_or_path ${BASE_MODEL_PATH} \
-#     --use_flash_attn \
-#     --tokenizer_name ${BASE_MODEL_PATH} \
-#     --use_slow_tokenizer \
-#     --train_file ${TRAINING_DATA_DIR}tulu_v2_data.jsonl \
-#     --max_seq_length 2048 \
-#     --preprocessing_num_workers 16 \
-#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
-#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-#     --learning_rate 2e-5 \
-#     --lr_scheduler_type linear \
-#     --warmup_ratio 0.03 \
-#     --weight_decay 0. \
-#     --num_train_epochs 2 \
-#     --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/baselines/llama_2_7b-tulu_v2_full/ \
-#     --logging_steps 1
+# BASELINE: Tulu 2 7B
+accelerate launch \
+    --mixed_precision bf16 \
+    --num_machines 1 \
+    --num_processes $NUM_GPUS \
+    --use_deepspeed \
+    --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+    open_instruct/finetune.py \
+    --model_name_or_path ${BASE_MODEL_PATH} \
+    --use_flash_attn \
+    --tokenizer_name ${BASE_MODEL_PATH} \
+    --use_slow_tokenizer \
+    --train_file ${TRAINING_DATA_DIR}tulu_v2_data.jsonl \
+    --max_seq_length 2048 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+    --learning_rate 2e-5 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.03 \
+    --weight_decay 0. \
+    --num_train_epochs 2 \
+    --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/baselines/llama_2_7b-tulu_v2_full/ \
+    --logging_steps 1
