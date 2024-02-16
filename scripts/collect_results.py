de02f85f28bedc2832d052c07a5c5cbb068fa873
jacob-morrison <jacobm@allenai.org>
2024-02-16 10:24:03 -0800

    test

--- scripts/collect_results.py
+++ scripts/collect_results.py
@@ -2,5 +2,7 @@ import json
 import os
 
 domain_adaptation_path = "/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/results/domain_addition/"
-tulu_evals = os.listdir(domain_adaptation_path)#.remove("gsm_cot").remove("science")
+tulu_evals = os.listdir(domain_adaptation_path)
+tulu_evals.remove("gsm_cot")
+tulu_evals.remove("science")
 print(tulu_evals)
