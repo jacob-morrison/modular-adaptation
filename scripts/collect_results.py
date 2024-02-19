8e3e10925a6cd46ca77575de4c7eda248783af37
jacob-morrison <jacobm@allenai.org>
2024-02-19 15:48:44 -0800

    .

--- scripts/collect_results.py
+++ scripts/collect_results.py
@@ -3,7 +3,7 @@ import os
 
 domain_adaptation_path = "/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/results/domain_addition/"
 tulu_evals = os.listdir(domain_adaptation_path)
-# tulu_evals.remove("gsm_cot")
+tulu_evals.remove("llama_2_7b-tulu_no_science") # can't delete this for some reason
 tulu_evals.remove("science")
 tulu_evals.remove("collected")
 
