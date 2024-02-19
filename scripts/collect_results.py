15af20a01a40978fc1f7a69db7eea9d7e8a74ff8
jacob-morrison <jacobm@allenai.org>
2024-02-19 15:47:38 -0800

    .

--- scripts/collect_results.py
+++ scripts/collect_results.py
@@ -3,7 +3,7 @@ import os
 
 domain_adaptation_path = "/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/results/domain_addition/"
 tulu_evals = os.listdir(domain_adaptation_path)
-tulu_evals.remove("gsm_cot")
+# tulu_evals.remove("gsm_cot")
 tulu_evals.remove("science")
 tulu_evals.remove("collected")
 
