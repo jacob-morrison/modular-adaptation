59df5f29858cc799c3728d9ec54ee98eef2c9a64
jacob-morrison <jacobm@allenai.org>
2024-02-22 11:36:42 -0800

    merge more models

--- scripts/mergekit-configs/merge_for_evals.py
+++ scripts/mergekit-configs/merge_for_evals.py
@@ -18,12 +18,12 @@ weights = [
 ]
 
 yaml_files = [
-    "scripts/mergekit-configs/merge-daves-tulu-and-science-200-linear-weighted.yml",
-    "scripts/mergekit-configs/merge-daves-tulu-and-science-1000-linear-weighted.yml",
-    "scripts/mergekit-configs/merge-daves-tulu-and-science-2500-linear-weighted.yml"
+    "scripts/mergekit-configs/merge-tulu-and-science-200-linear-weighted.yml",
+    "scripts/mergekit-configs/merge-tulu-and-science-1000-linear-weighted.yml",
+    "scripts/mergekit-configs/merge-tulu-and-science-2500-linear-weighted.yml"
 ]
 
-output_dir = "/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/domain_addition/with_daves_tulu_model"
+output_dir = "/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/domain_addition/another_tulu_only_model"
 
 for yaml_file in yaml_files:
     if '200' in yaml_file:
@@ -43,7 +43,7 @@ for yaml_file in yaml_files:
         d["models"][1]["parameters"]["weight"] = tuluWeight
 
         # merge models
-        name = f"merge-daves-tulu-{tuluWeight}-science-{num_science}-{scienceWeight}"
+        name = f"merge-my-new-tulu-{tuluWeight}-science-{num_science}-{scienceWeight}"
         fn = f"scripts/mergekit-configs/auto_created/{name}.yaml"
         file = open(fn, "w")
         yaml.dump(d, file, default_flow_style=True)
