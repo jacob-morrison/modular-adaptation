5b867c12d12bc3e03f18199123a3b2b75f9aaaac
jacob-morrison <jacobm@allenai.org>
2024-04-16 10:12:42 -0700

    merge

--- scripts/merge_models/create-merge-session.sh
+++ scripts/merge_models/create-merge-session.sh
@@ -12,3 +12,10 @@ beaker session create --gpus 1 -d --budget ai2/allennlp \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_20=/safety_20 \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_60=/safety_60 \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_100=/safety_100
+
+	
+
+beaker session create --gpus 1 -d --budget ai2/oe-adapt \
+    --mount beaker://jacobm/llama_2_7b-tulu_all-safety_none=/tulu_all \
+    --mount beaker://jacobm/llama_2_7b-tulu_none-safety_v0_100=/safety_v0_100
+    --mount beaker://jacobm/llama_2_7b-tulu_none-safety_10=/safety_10 \
