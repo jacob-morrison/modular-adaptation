45c59e1f3bec026561bffd565b43a8a6a9265e50
jacob-morrison <jacobm@allenai.org>
2024-04-26 11:21:04 -0700

    safety

--- scripts/merge_models/create-merge-session.sh
+++ scripts/merge_models/create-merge-session.sh
@@ -1,4 +1,4 @@
-beaker session create --gpus 1 -d --budget ai2/allennlp \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-science_none-seed_42_4096=/tulu_all \
     --mount beaker://jacobm/llama_2_7b-tulu_none-science_100_4096=/science_100 \
     --mount beaker://jacobm/llama_2_7b-tulu_none-science_200_4096=/science_200 \
@@ -11,31 +11,31 @@ beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-science_none-seed_42_4096=/tulu_all \
     --mount beaker://jacobm/llama_2_7b-tulu_none-science_2500_4096=/science_2500 \
 
-beaker session create --gpus 1 -d --budget ai2/oe-adapt \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-safety_none=/tulu_all \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_10=/safety_10 \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_20=/safety_20 \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_60=/safety_60 \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_100=/safety_100
 
-beaker session create --gpus 1 -d --budget ai2/allennlp \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-safety_none=/tulu_all \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_100=/safety_100
 
-beaker session create --gpus 1 -d --budget ai2/allennlp \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-safety_none=/tulu_all \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_10=/safety_10
 
-beaker session create --gpus 1 -d --budget ai2/oe-adapt \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-safety_none=/tulu_all \
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_10=/safety_10
     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_v0_100=/safety_v0_100
 
-beaker session create --gpus 1 -d --budget ai2/oe-adapt \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-safety_none=/tulu_all \
     --mount beaker://jacobm/tulu_2_7b_uncensored-tulu_none-safety_100=/tulu_2_7b_uncensored_safety_100
 
 
-beaker session create --gpus 1 -d --budget ai2/oe-adapt \
+beaker session create --gpus 1 --budget ai2/oe-adapt \
     --mount beaker://jacobm/llama_2_7b-tulu_all-science_none-seed_42_4096=/tulu_all \
     --mount beaker://jacobm/tulu_2_7b_continued_ft-tulu_none-science_2500_4096=/tulu_2_7b_science_2500
--- scripts/merge_models/merge-and-upload-safety.py
+++ scripts/merge_models/merge-and-upload-safety.py
@@ -3,53 +3,45 @@ import os
 import subprocess
 import yaml
 
-weights = [
-    (0.1, 0.9),
-    (0.2, 0.8),
-    (0.3, 0.7),
-    (0.4, 0.6),
-    (0.5, 0.5),
-    (0.6, 0.4),
-    (0.7, 0.3),
-    (0.8, 0.2),
-    (0.9, 0.1),
-]
-
 # weights = [
-#     (1.0, 1.0),
-#     (1.0, 0.9),
-#     (1.0, 0.8),
-#     (1.0, 0.7),
-#     (1.0, 0.6),
-#     (1.0, 0.5),
-#     (1.0, 0.4),
-#     (1.0, 0.3),
-#     (1.0, 0.2),
-#     (1.0, 0.1),
+#     (0.1, 0.9),
+#     (0.2, 0.8),
+#     (0.3, 0.7),
+#     (0.4, 0.6),
+#     (0.5, 0.5),
+#     (0.6, 0.4),
+#     (0.7, 0.3),
+#     (0.8, 0.2),
+#     (0.9, 0.1),
 # ]
 
-# by data amounts
 weights = [
-    # LINEAR WEIGHTED!!!
-    # (0.98, 0.02) # safety 10
-    # (0.96, 0.04) # safety 20
-    # (0.88, 0.12) # safety 60
-    # (0.82, 0.18), # safety 100
-
-    # (1.0, 0.02), # safety 10
-    # (1.0, 0.04), # safety 20
-    # (1.0, 0.13), # safety 60
-    # (1.0, 0.22), # safety 100
+    (1.0, 1.0),
+    (1.0, 0.9),
+    (1.0, 0.8),
+    (1.0, 0.7),
+    (1.0, 0.6),
+    (1.0, 0.5),
+    (1.0, 0.4),
+    (1.0, 0.3),
+    (1.0, 0.22), # safety 100
+    (1.0, 0.2),
+    (1.0, 0.1),
 ]
 
-# science_files = {
-#     "100": "/science_100",
-#     # "200": "/science_200",
-#     # "500": "/science_500",
-#     # "1000": "/science_1000",
-#     # "2500": "/science_2500",
-#     # "upsample": "/science_upsample"
-# }
+# by data amounts
+# weights = [
+#     # LINEAR WEIGHTED!!!
+#     # (0.98, 0.02) # safety 10
+#     # (0.96, 0.04) # safety 20
+#     # (0.88, 0.12) # safety 60
+#     # (0.82, 0.18), # safety 100
+
+#     # (1.0, 0.02), # safety 10
+#     # (1.0, 0.04), # safety 20
+#     # (1.0, 0.13), # safety 60
+#     # (1.0, 0.22), # safety 100
+# ]
 
 safety_files = {
     # "uncensored_100": "/tulu_2_7b_uncensored_safety_100",
@@ -64,10 +56,11 @@ merge_methods = [
     # "linear_weighted",
     # "task_arithmetic",
     # "dare_task_arithmetic",
-    "dare_linear",
-    "dare_ties",
-    "ties",
-    "slerp",
+    # "dare_linear",
+    # "dare_ties",
+    # "ties",
+    # "slerp",
+    "dare_task_arithmetic",
 ]
 
 tulu_file = "/tulu_all"
@@ -92,7 +85,7 @@ for merge_method in merge_methods:
                 d["models"][0]["parameters"]["weight"] = tuluWeight
                 d["models"][1]["model"] = safety_files[safety_amount]
                 d["models"][1]["parameters"]["weight"] = safetyWeight
-            elif merge_method in ["dare_linear", "dare_ties", "ties"]:
+            elif merge_method in ["dare_linear", "dare_ties", "ties", "dare_task_arithmetic"]:
                 # Set merge-specific parameters
                 d["models"][1]["model"] = tulu_file
                 d["models"][1]["parameters"]["weight"] = tuluWeight
