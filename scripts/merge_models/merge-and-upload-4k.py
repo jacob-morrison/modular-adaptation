f7c2cc250fda847584fa33057024570495cdd6cc
jacob-morrison <jacobm@allenai.org>
2024-05-07 23:35:04 +0200

    merge

--- scripts/merge_models/merge-and-upload-4k.py
+++ scripts/merge_models/merge-and-upload-4k.py
@@ -11,6 +11,11 @@ import yaml
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_50=/coding_50 \
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_100=/coding_100
 
+# beaker session create --gpus 1 --budget ai2/oe-adapt  \
+#     --mount beaker://jacobm/llama_2_7b-tulu_all_no_science_no_safety_no_coding=/llama_2_7b-tulu_all_no_science_no_safety_no_coding \
+#     --mount beaker://jacobm/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-coding_100=/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-coding_100 \
+#     --mount beaker://jacobm/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-safety_100=/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-safety_100 \
+#     --mount beaker://jacobm/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-science_2500=/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-science_2500
 
 weights = [
     (0.1, 0.9),
@@ -40,14 +45,14 @@ weights = [
 # ]
 
 domain_models = {
-    "safety_100": "/llama_2_7b-tulu_none-safety_100-4k",
-    "coding_100": "/llama_2_7b-tulu_none-coding_100-4k",
-    "science_2500": "/llama_2_7b-tulu_none-science_2500-4k",
+    "tulu_2_7b_coding_100": "/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-coding_100",
+    "tulu_2_7b_safety_100": "/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-safety_100",
+    "tulu_2_7b_science_2500": "/tulu_2_7b_no_science_no_safety_no_coding-tulu_none-science_2500",
 }
 
 merge_methods = [
     "linear_weighted",
-    "task_arithmetic",
+    # "task_arithmetic",
 ]
 
 tulu_file = "/llama_2_7b-tulu_all_no_science_no_safety_no_coding"
