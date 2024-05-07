743257de4606119f5ba91af349f209bdfd172aeb
jacob-morrison <jacobm@allenai.org>
2024-05-07 06:18:01 +0200

    match

--- scripts/make-tulu-mix-no-domains.py
+++ scripts/make-tulu-mix-no-domains.py
@@ -54,38 +54,41 @@ with open(coding_only_path) as f_in:
     for line in f_in.readlines():
         coding_only_examples.append(json.loads(line))
 
-tulu_1_and_science = science_only_examples + tulu_no_science_no_safety
+random.shuffle(tulu_no_science_no_safety)
+random.shuffle(tulu_no_science_no_safety_no_coding)
+
+tulu_1_and_science = science_only_examples + tulu_no_science_no_safety[:len(science_only_examples)]
 random.shuffle(tulu_1_and_science)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety-science_2500.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_match_no_science_no_safety-science_2500.jsonl", "w") as f_out:
     for elem in tulu_1_and_science:
         print(json.dumps(elem), file=f_out)
 
-tulu_2_and_science = science_only_examples + tulu_no_science_no_safety_no_coding
+tulu_2_and_science = science_only_examples + tulu_no_science_no_safety_no_coding[:len(science_only_examples)]
 random.shuffle(tulu_2_and_science)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety_no_coding-science_2500.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_match_no_science_no_safety_no_coding-science_2500.jsonl", "w") as f_out:
     for elem in tulu_2_and_science:
         print(json.dumps(elem), file=f_out)
 
-tulu_1_and_safety = safety_only_examples + tulu_no_science_no_safety
+tulu_1_and_safety = safety_only_examples + tulu_no_science_no_safety[:len(safety_only_examples)]
 random.shuffle(tulu_1_and_safety)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety-safety_100.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_match_no_science_no_safety-safety_100.jsonl", "w") as f_out:
     for elem in tulu_1_and_safety:
         print(json.dumps(elem), file=f_out)
 
-tulu_2_and_safety = safety_only_examples + tulu_no_science_no_safety_no_coding
+tulu_2_and_safety = safety_only_examples + tulu_no_science_no_safety_no_coding[:len(safety_only_examples)]
 random.shuffle(tulu_2_and_safety)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety_no_coding-safety_100.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_match_no_science_no_safety_no_coding-safety_100.jsonl", "w") as f_out:
     for elem in tulu_2_and_safety:
         print(json.dumps(elem), file=f_out)
 
-tulu_1_and_coding = coding_only_examples + tulu_no_science_no_safety
+tulu_1_and_coding = coding_only_examples + tulu_no_science_no_safety[:len(coding_only_examples)]
 random.shuffle(tulu_1_and_coding)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety-coding_100.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_match_no_science_no_safety-coding_100.jsonl", "w") as f_out:
     for elem in tulu_1_and_coding:
         print(json.dumps(elem), file=f_out)
         
-tulu_2_and_coding = coding_only_examples + tulu_no_science_no_safety_no_coding
+tulu_2_and_coding = coding_only_examples + tulu_no_science_no_safety_no_coding[:len(coding_only_examples)]
 random.shuffle(tulu_2_and_coding)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety_no_coding-coding_100.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_match_no_science_no_safety_no_coding-coding_100.jsonl", "w") as f_out:
     for elem in tulu_2_and_coding:
         print(json.dumps(elem), file=f_out)
