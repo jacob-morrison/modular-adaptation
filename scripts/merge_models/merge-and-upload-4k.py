cfb47d553aefa6b5e96349dd5cd5d0c31a6ec42f
jacob-morrison <jacobm@allenai.org>
2024-05-10 23:01:16 -0700

    coding weighted ft

--- scripts/merge_models/merge-and-upload-4k.py
+++ scripts/merge_models/merge-and-upload-4k.py
@@ -45,7 +45,7 @@ import yaml
 
 weights = [
     # coding
-    # (0.64, 0.36),
+    (0.64, 0.36),
     # (1.0, 0.57),
 
     # safety
@@ -53,16 +53,16 @@ weights = [
     # (1.0, 0.24),
 
     # science
-    (0.82, 0.18),
-    (1.0, 0.22),
+    # (0.82, 0.18),
+    # (1.0, 0.22),
 ]
 
 domain_models = {
     # "coding_100": "/llama_2_7b-coding_100",
     # "safety_100": "/llama_2_7b-safety_100",
-    "science_2500": "/llama_2_7b-science_2500",
+    # "science_2500": "/llama_2_7b-science_2500",
 
-    # "tulu_2_7b_coding_100": "/tulu_2_7b-tulu_none-coding_100",
+    "tulu_2_7b_coding_100": "/tulu_2_7b-tulu_none-coding_100",
     # "tulu_2_7b_safety_100": "/tulu_2_7b-tulu_none-safety_100",
     # "tulu_2_7b_science_2500": "/tulu_2_7b-tulu_none-science_2500",
 }
