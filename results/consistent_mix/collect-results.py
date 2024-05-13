ecf05c056f7b4eb2eac4b7880f1d550a520566a0
jacob-morrison <jacobm@allenai.org>
2024-05-13 16:53:36 -0700

    .

--- results/consistent_mix/collect-results.py
+++ results/consistent_mix/collect-results.py
@@ -364,6 +364,7 @@ with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/results/domain
 
 df = pd.DataFrame(data_map.values())
 print(df)
+print(df["domain_model"])
 df.to_csv("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/results/domain_addition/consistent_mix/results.csv", index=False)
 
 # df_safety_evals = pd.DataFrame(safety_eval_data)
