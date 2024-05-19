e8cfa0f86b440c0fb56beb8369d62cfd0325320b
jacob-morrison <jacobm@allenai.org>
2024-05-18 17:24:14 -0700

    .

---
+++ scripts/make-smaller-mixes.py
@@ -0,0 +1,24 @@
+import json
+import random
+
+coding_only_path = "/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/coding/tulu_none-coding_100.jsonl"
+
+coding_examples = []
+with open(coding_only_path) as f_in:
+    for line in f_in.readlines():
+        coding_examples.append(json.loads(line)) 
+
+random.shuffle(coding_examples)
+
+amounts = [
+    20,
+    40,
+    60,
+    80,
+]
+
+for amt in amounts:
+    current_coding_examples = coding_examples[:len(coding_examples) * amt / 100.]
+    with open(f"/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/coding/tulu_none-coding_{amt}.jsonl", "w") as f_out:
+        for elem in current_coding_examples:
+            print(json.dumps(elem), file=f_out)
