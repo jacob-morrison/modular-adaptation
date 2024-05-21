51e18d4cbfa1420e65ef3b591dde093ac7512426
jacob-morrison <jacobm@allenai.org>
2024-05-21 11:47:56 -0700

    .

--- scripts/finetune_7b_tpu-1.sh
+++ scripts/finetune_7b_tpu-1.sh
@@ -30,12 +30,12 @@ gcloud alpha compute tpus tpu-vm ssh jacobm-v3-128 --zone=us-east1-d --project=a
     --optimizer.accumulate_gradient_steps=4 \
     --train_dataset.type='tulu_json_torch' \
     --train_dataset.text_processor.fields='[prompt],completion' \
-    --train_dataset.json_torch_dataset.path='gs://jacobm-bucket/modular_adaptation/training_data/consistent_mix/tulu_none-coding_20.jsonl' \
+    --train_dataset.json_torch_dataset.path='gs://jacobm-bucket/modular_adaptation/training_data/consistent_mix/tulu_none-coding_60.jsonl' \
     --train_dataset.json_torch_dataset.seq_length=4096 \
     --train_dataset.json_torch_dataset.batch_size=32  \
     --checkpointer.save_optimizer_state=False \
     --logger.online=True --logger.entity='jacobmai2' --logger.project='train-big-llamas-on-tpus' \
-    --logger.output_dir='gs://jacobm-bucket/modular_adaptation/checkpoints/consistent_mix/tulu_2_7b_with_coding-tulu_none-coding_20' &> all.log &"
+    --logger.output_dir='gs://jacobm-bucket/modular_adaptation/checkpoints/consistent_mix/tulu_2_7b_with_coding-tulu_none-coding_60' &> all.log &"
 
     # --load_checkpoint='params::gs://hamishi-east1/easylm/llama2/7b' \
     # --load_checkpoint='params::gs://jacobm-bucket/modular_adaptation/checkpoints/consistent_mix/llama_2_7b-tulu_all/44fd8533fde84a2e8a117e4bbc2c89e0/streaming_params_17208' \
