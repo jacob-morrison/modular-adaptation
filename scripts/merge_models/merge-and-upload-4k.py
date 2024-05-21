007a06095c9ee9b5829b6fd53372f470254aaaa8
jacob-morrison <jacobm@allenai.org>
2024-05-20 20:08:31 -0700

    fix

--- scripts/merge_models/merge-and-upload-4k.py
+++ scripts/merge_models/merge-and-upload-4k.py
@@ -38,6 +38,10 @@ import yaml
 
 # beaker session create --gpus 1 --budget ai2/oe-adapt  \
 #     --mount beaker://jacobm/llama_2_7b-tulu_all=/llama_2_7b-tulu_all \
+#     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_20=/llama_2_7b-tulu_none-coding_20 \
+#     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_40=/llama_2_7b-tulu_none-coding_40 \
+#     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_60=/llama_2_7b-tulu_none-coding_60 \
+#     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_80=/llama_2_7b-tulu_none-coding_80 \
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_20=/llama_2_7b-tulu_none-safety_20 \
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_40=/llama_2_7b-tulu_none-safety_40 \
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_60=/llama_2_7b-tulu_none-safety_60 \
@@ -115,10 +119,15 @@ domain_models = {
     # "science_500": "/llama_2_7b-tulu_none-science_500",
     # "science_1000": "/llama_2_7b-tulu_none-science_1000",
 
-    "safety_20": "/llama_2_7b-tulu_none-safety_20",
-    "safety_40": "/llama_2_7b-tulu_none-safety_40",
-    "safety_60": "/llama_2_7b-tulu_none-safety_60",
-    "safety_80": "/llama_2_7b-tulu_none-safety_80",
+    # "safety_20": "/llama_2_7b-tulu_none-safety_20",
+    # "safety_40": "/llama_2_7b-tulu_none-safety_40",
+    # "safety_60": "/llama_2_7b-tulu_none-safety_60",
+    # "safety_80": "/llama_2_7b-tulu_none-safety_80",
+
+    "coding_20": "/llama_2_7b-tulu_none-coding_20",
+    "coding_40": "/llama_2_7b-tulu_none-coding_40",
+    "coding_60": "/llama_2_7b-tulu_none-coding_60",
+    "coding_80": "/llama_2_7b-tulu_none-coding_80",
 
     # "science_100": "/tulu_2_7b-tulu_none-science_100",
     # "science_200": "/tulu_2_7b-tulu_none-science_200",
