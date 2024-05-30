22e156063dbc8482e9ecae8814e455c6b0f2b6d7
jacob-morrison <jacobm@allenai.org>
2024-05-29 19:28:55 -0700

    .

--- scripts/merge_models/merge-and-upload-4k.py
+++ scripts/merge_models/merge-and-upload-4k.py
@@ -137,6 +137,12 @@ import yaml
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-safety_100=/llama_2_7b-tulu_none-safety_100 \
 #     --mount beaker://jacobm/llama_2_7b-tulu_none-coding_100=/llama_2_7b-tulu_none-coding_100
 
+# beaker session create --gpus 1 --budget ai2/oe-adapt  \
+#     --mount beaker://jacobm/llama_2_7b-tulu_all=/llama_2_7b-tulu_all \
+#     --mount beaker://jacobm/tulu_2_7b-tulu_none-science_2500=/tulu_2_7b-tulu_none-science_2500 \
+#     --mount beaker://jacobm/tulu_2_7b-tulu_none-coding_100=/tulu_2_7b-tulu_none-coding_100 \
+#     --mount beaker://jacobm/tulu_2_7b-tulu_none-safety_100=/tulu_2_7b-tulu_none-safety_100 
+
 # weights = [
 #     # (0.1, 0.9),
 #     # (0.2, 0.8),
