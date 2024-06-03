99e8116df00a630e8321d1ee28e75e5c3279a8ea
jacob-morrison <jacobm@allenai.org>
2024-06-02 20:51:19 -0700

    .

--- scripts/make-combo-mixtures.py
+++ scripts/make-combo-mixtures.py
@@ -16,19 +16,14 @@ with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/saf
     for line in f_in.readlines():
         safety_examples.append(json.loads(line))
 
-code_examples = []
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/coding/tulu_none-coding_100.jsonl") as f_in:
-    for line in f_in.readlines():
-        code_examples.append(json.loads(line))
-
-three_domains = science_examples + safety_examples + code_examples
+three_domains = science_examples + safety_examples
 random.shuffle(three_domains)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_none-science_2500-safety_100-coding_100.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_none-science_2500-safety_100.jsonl", "w") as f_out:
     for elem in three_domains:
         print(json.dumps(elem), file=f_out)
 
 tulu_and_three_domains = tulu_all_no_science_no_safety_no_coding + three_domains
 random.shuffle(tulu_and_three_domains)
-with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety_no_coding-science_2500-safety_100-coding_100.jsonl", "w") as f_out:
+with open("/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/train_data/consistent_mix/tulu_all_no_science_no_safety_no_coding-science_2500-safety_100.jsonl", "w") as f_out:
     for elem in tulu_and_three_domains:
         print(json.dumps(elem), file=f_out)
